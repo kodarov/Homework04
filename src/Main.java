4a00fced46f538abdaecda859fea217c771fa6d3
kodarov_salavat <kodarov@gmail.com>
2022-12-27 00:42:41 +0400

    hw04/task03

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -14,5 +15,11 @@ public class Main {
         int temp=7;
         if (temp>=5) System.out.println("Сегодня на улице " + temp + " градусов, можно идти без шапки");
         else System.out.println("Сегодня на улице " + temp + " градусов, нужно надеть шапку");
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int speed=70;
+        if (speed>60) System.out.println("Если скорость " + speed + " придется заплатить штраф");
+        else System.out.println("Если скорость " + speed + " можно ездить спокойно");
     }
 }
