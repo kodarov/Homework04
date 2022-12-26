ed718c18e2aea7f4797ef216201623eaa5f1878c
kodarov_salavat <kodarov@gmail.com>
2022-12-27 00:35:18 +0400

    hw04/task02

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) System.out.println("Если возраст человека равен "+age+" он совершеннолетний");
         else System.out.println("Если возраст человека равен "+age+" он не достиг совершеннолетия, нужно немного подождать");
+    }
+    public static void task2() {
+        int temp=7;
+        if (temp>=5) System.out.println("Сегодня на улице " + temp + " градусов, можно идти без шапки");
+        else System.out.println("Сегодня на улице " + temp + " градусов, нужно надеть шапку");
     }
 }
